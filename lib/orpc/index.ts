b865af97c46c2cb6a11da12e2a44179febcc9837
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-09 15:17:25 +0200

    Add @t3-oss/env-nextjs dependency and update RPC link configuration
    - Introduced the @t3-oss/env-nextjs package to package.json and bun.lock for environment variable management.
    - Updated the RPC link in the ORPC client to use a dynamic base URL from the environment, enhancing flexibility for different environments.

--- lib/orpc/index.ts
+++ lib/orpc/index.ts
@@ -2,9 +2,10 @@ import { router } from "@/lib/orpc/router"
 import { createORPCClient } from "@orpc/client"
 import { RPCLink } from "@orpc/client/fetch"
 import { RouterClient } from "@orpc/server"
+import { env } from "../env/web"
 
 const rpcLink = new RPCLink({
-    url: "http://localhost:3000/rpc"
+    url: `${env.NEXT_PUBLIC_BASE_URL}/rpc`
 })
 
 export const api: RouterClient<typeof router> = createORPCClient(rpcLink)

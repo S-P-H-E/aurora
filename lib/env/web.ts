5bb3ba2a6f6bd99313077e3667539aecbbe9fb49
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-14 11:11:34 +0200

    change in env

--- lib/env/web.ts
+++ lib/env/web.ts
@@ -1,17 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
-
-console.log(`Base ${baseUrl}`)
-
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_BASE_URL: z.url(),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_BASE_URL: baseUrl,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
 });
--- lib/orpc/index.ts
+++ lib/orpc/index.ts
@@ -4,8 +4,12 @@ import { RPCLink } from "@orpc/client/fetch"
 import { RouterClient } from "@orpc/server"
 import { env } from "../env/web"
 
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+  ? `https://${env.NEXT_PUBLIC_BASE_URL}`
+  : "http://localhost:3000";
+
 const rpcLink = new RPCLink({
-    url: `${env.NEXT_PUBLIC_BASE_URL}/rpc`
+    url: `${baseUrl}/rpc`
 })
 
 export const api: RouterClient<typeof router> = createORPCClient(rpcLink)

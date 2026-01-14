1488a392d86ef3ceab65e3ca97049e40d4d790b2
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-14 11:17:16 +0200

    lets hope this works

--- lib/orpc/index.ts
+++ lib/orpc/index.ts
@@ -1,15 +1,15 @@
-import { router } from "@/lib/orpc/router"
-import { createORPCClient } from "@orpc/client"
-import { RPCLink } from "@orpc/client/fetch"
-import { RouterClient } from "@orpc/server"
-import { env } from "../env/web"
+import { router } from "@/lib/orpc/router";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { RouterClient } from "@orpc/server";
+import { env } from "../env/web";
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+const baseUrl = env.NEXT_PUBLIC_BASE_URL
   ? `https://${env.NEXT_PUBLIC_BASE_URL}`
   : "http://localhost:3000";
 
 const rpcLink = new RPCLink({
-    url: `${baseUrl}/rpc`
-})
+  url: `${baseUrl}/rpc`,
+});
 
-export const api: RouterClient<typeof router> = createORPCClient(rpcLink)
+export const api: RouterClient<typeof router> = createORPCClient(rpcLink);
